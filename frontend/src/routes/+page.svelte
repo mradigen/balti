4b47b03ef9c85a09859ea77a2efad82dcc8566c7
mradigen <55953083+mradigen@users.noreply.github.com>
2025-01-11 23:15:38 +0530

    add: view file uploads

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,15 +2,31 @@
 	import { uploadFile } from '$lib/minio';
 	import { shortenURL } from '$lib/short';
 	import { env } from '$lib/env';
+	import Github from '$lib/components/Github.svelte';
+	import { Expiry } from '$lib/expiry';
+	import UploadedItem from '$lib/components/UploadedItem.svelte';
 
-	const defaultExpiry = env.DEFAULT_EXPIRY;
+	type FileUploaded = {
+		name: string;
+		url: string;
+		expiry: Expiry;
+	};
 
 	let fileInput: HTMLInputElement;
+	let selectedExpiry = env.DEFAULT_EXPIRY;
+	let filesUploaded: FileUploaded[] = [];
 
 	async function upload(file: File) {
-		let longURL = await uploadFile(file, defaultExpiry);
+		const longURL = await uploadFile(file, selectedExpiry);
 		if (longURL == null) return alert('Error');
-		console.log(await shortenURL(longURL));
+		const shortURL = await shortenURL(longURL);
+		if (shortURL == null) return alert('Error');
+		filesUploaded.push({
+			name: file.name,
+			url: shortURL,
+			expiry: selectedExpiry
+		});
+		filesUploaded = filesUploaded; // Trigger an update
 	}
 
 	export function fileDropped(e: DragEvent) {
@@ -33,14 +49,38 @@
 	}
 </script>
 
-<div class="flex h-screen w-screen items-center justify-center">
-	<input type="file" class="hidden" bind:this={fileInput} on:change={fileInputUpload} />
-	<button
-		class="flex h-1/2 w-1/2 items-center justify-center rounded-lg border-2 border-dashed border-gray-500 bg-gray-100 hover:cursor-pointer hover:bg-gray-200"
-		on:click={() => fileInput.click()}
-		on:drop|preventDefault={fileDropped}
-		on:dragover|preventDefault
+<div class="m-auto flex w-fit min-w-[50vh] flex-col space-y-16">
+	<div class="flex flex-col items-center space-y-8 font-extrabold">
+		<Github></Github>
+
+		<input type="file" class="hidden" bind:this={fileInput} on:change={fileInputUpload} />
+		<button
+			class="flex h-[50vh] w-[50vw] items-center justify-center rounded-lg border-2 border-dashed border-gray-500 bg-gray-100 hover:cursor-pointer hover:bg-gray-200"
+			on:click={() => fileInput.click()}
+			on:drop|preventDefault={fileDropped}
+			on:dragover|preventDefault
+		>
+			<h1 class="text-center text-5xl">Drop your file here</h1>
+		</button>
+
+		<div class="w-fit">
+			<label for="expiry" class="block text-lg text-gray-700">Select Expiry Date</label>
+			<select
+				id="expiry"
+				class="mt-1 block w-full rounded-md border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+				bind:value={selectedExpiry}
+			>
+				{#each Object.entries(Expiry) as [key, value]}
+					<option {value}>{key.charAt(0).toUpperCase() + key.slice(1)}</option>
+				{/each}
+			</select>
+		</div>
+	</div>
+	<div
+		class="flex grid-cols-3 flex-col items-center space-y-1 overflow-hidden rounded-md font-extrabold"
 	>
-		<h1 class="text-center text-5xl font-extrabold">Drop your file here</h1>
-	</button>
+		{#each filesUploaded as file}
+			<UploadedItem name={file.name} url={file.url} expiry={file.expiry}></UploadedItem>
+		{/each}
+	</div>
 </div>
