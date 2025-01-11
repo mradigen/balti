ed8319643a0d293338d15f6f5d5bd6fe75bdba9f
mradigen <55953083+mradigen@users.noreply.github.com>
2025-01-11 23:26:03 +0530

    fix: grammar

--- frontend/src/lib/components/UploadedItem.svelte
+++ frontend/src/lib/components/UploadedItem.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Expiry } from '$lib/expiry';
+	import { Expiry } from '$lib/expiry';
 
 	export let name: string;
 	export let url: string;
@@ -9,5 +9,5 @@
 <div class="grid w-full grid-cols-3 justify-between bg-gray-100 p-4 text-center">
 	<p class="text-left">{name}</p>
 	<a href={url}>{url}</a>
-	<p class="text-right">{expiry.split('d')[0] + ' days'}</p>
+	<p class="text-right">{expiry.split('d')[0] + (expiry == Expiry.day ? ' day' : ' days')}</p>
 </div>
